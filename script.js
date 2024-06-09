d16dd2adaf305640cc956df47de7371728228ddd
Poppyland937 <154359109+Poppyland937@users.noreply.github.com>
2024-06-09 18:34:03 +0100

    updated css and javascript

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function findCity(event) {
+function submitSearch(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  let city = (cityElement.innerHTML = searchInputElement.value);
+  let searchInput = document.querySelector("#search-form-input");
+  let cityPlace = document.querySelector("#weather-city");
+  let city = (cityPlace.innerHTML = searchInput.value);
   fetchWeather(city);
 }
 function fetchWeather(city) {
@@ -12,13 +12,55 @@ function fetchWeather(city) {
   console.log(url);
 }
 function displayTemperature(response) {
-  let cityTemperature = document.querySelector("#weather-temperature");
+  let temperatureNumber = document.querySelector("#weather-temperature");
   let temperature = Math.round(response.data.temperature.current);
-  cityTemperature.innerHTML = temperature;
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  let icon = document.querySelector("#weather-icon");
+  icon.innerHTML = `<img src="${response.data.condition.icon_url} " class="weather-icon"> `;
+
+  temperatureNumber.innerHTML = temperature;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}Km/h`;
 }
+
+function formatDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${hour}:${minute}`;
+}
+
+function initialize() {
+  let defaultCity = "Paris";
+  fetchWeather(defaultCity);
+}
+
 function handleError(error) {
   console.log("Error fetching weather data:", error);
   alert("Unable to retrieve weather data. Please try again later.");
 }
+
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", findCity);
+searchFormElement.addEventListener("submit", submitSearch);
+
+window.onload = initialize;
